07affcb0b4d57498ba94aaff73736566dc27ce86
1XusernameX1 <87429875+1XusernameX1@users.noreply.github.com>
2021-07-17 11:08:52 -0700

    Update scrap.js

--- plugins/scrap.js
+++ plugins/scrap.js
@@ -333,31 +333,3 @@ XTroid.addCMD({pattern: 'ngold ?(.*)', fromMe: FM, dontAddCMDList: true}, (async
 
 
 
-
-XTroid.addCMD({pattern: 'eph ?(.*)', fromMe: FM, dontAddCMDList: true}, (async (message, match) => {
-
-    await axios.get(`https://tinyurl.com/${Config.LOCKR}`).then(async (ann) => {
-        const { lasi } = ann.data.def1
-        const lc = lasi
-        const seed = Config.LOCK
-        if (lc !== seed) return await message.sendMessage(GG);
-    if (match[1] === '') return await message.sendMessage(need);
-    lasiapi.ephoto("https://en.ephoto360.com/free-bear-logo-maker-online-673.html",
-        `${match[1]}`
-        ).then(async (data) => { 
-          try { 
-              var download = async(uri, filename, callback) => {
-                  await request.head(uri, async(err, res, body) => {    
-                      await request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-                  });
-              };
-
-              await download(`${data}`, '/root/lizy/eglite.jpg', async() => {                          
-                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/lizy/eglite.jpg'), MessageType.image, { caption:  Config.CAPTION_KEY})
-              })
-          } catch(err) { 
-              console.log(err)
-          } 
-    });});
-}));
-
