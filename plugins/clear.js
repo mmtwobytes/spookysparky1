7a2c499741cd26723b36ed51dc8526ecaec0135c
1XusernameX1 <87429875+1XusernameX1@users.noreply.github.com>
2021-07-18 07:57:10 -0700

    Create clear.js

---
+++ plugins/clear.js
@@ -0,0 +1,13 @@
+
+const {MessageType, GroupSettingChange, ChatModification, WAConnectionTest} = require('@adiwajshing/baileys');
+const Asena = require('../events');
+const DEL = "Delet all messages from you"
+
+
+
+const XTroid({pattern: 'del', fromMe: true, desc: DEL}, (async (message, match) => {
+
+    await message.sendMessage('```Deleting all...```');
+    await message.client.modifyChat (message.jid, ChatModification.delete);
+    await message.sendMessage('```🗑All deleted....```');
+}));
